76fe4433119ec23883232f60a2f1070ae15cad10
meszaros-adam <meszarosadam@outlook.hu>
2022-11-29 20:01:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# reataurant_webpage
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header>Az én éttermem</header>
+    <main>tartalom</main>
+    <footer>Lábléc</footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  background: #1F2F16;
+  margin: 0;
+}
+header{
+  background: #5E5C6C;
+  padding: 1rem;
+}
+footer {
+  position: absolute;
+  bottom: 0px;
+  background: #5E5C6C;
+  width: 100vw;
+  height: 3rem;
+  display: grid;
+  place-items: center;
+}
+</style>
