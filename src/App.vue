8bb339c47b2cef6fe5113910211aaa285d3e291a
meszaros-adam <meszarosadam@outlook.hu>
2022-12-01 19:00:23 +0100

    dynamic year in the footer, new font family, new colors

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,62 @@
 <template>
   <div id="app">
-    <header>Az én éttermem</header>
+    <header>Kitalált Étterem</header>
     <main>tartalom</main>
-    <footer>Lábléc</footer>
+    <footer> &#169; {{ year }}</footer>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue'
 
 export default {
   name: 'app',
   components: {
+  },
+  setup() {
+    const year = computed(() => {
+      return new Date().getFullYear()
+    })
+
+    return { year }
+
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap');
+
+:root {
+  --mainColor: #6B5E62;
+  --secondaryColor: #D5CAD6;
+  --shadow: 2px 2px grey;
+}
+
 body {
-  background: #1F2F16;
+  font-family: 'Roboto Mono', monospace;
+  background: var(--secondaryColor);
   margin: 0;
 }
-header{
-  background: #5E5C6C;
-  padding: 1rem;
+
+header {
+  background: var(--mainColor);
+  padding: 0, 5rem;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 50px;
+  text-shadow: var(--shadow);
+
 }
+
 footer {
   position: absolute;
   bottom: 0px;
-  background: #5E5C6C;
+  background: var(--mainColor);
   width: 100vw;
   height: 3rem;
   display: grid;
   place-items: center;
+  font-weight: bolder;
 }
 </style>
