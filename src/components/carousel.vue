e81fe0987da8d5ad3ca4c4c56b15b41849e78b63
meszaros-adam <meszarosadam@outlook.hu>
2022-12-21 14:24:15 +0100

    fix step length in carousel

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="carousel-container">
         <div class="carousel">
-            <div class="inner" :style="[innerStyles]" ref="inner">
-                <div class="card" v-for="card in cards" :key="card">
-                    {{ card }}
+            <div class="inner" :style="[innerStyles]" id="inner">
+                <div class="card" v-for="(image, i) in images" :key="i">
+                    <img :src="image" alt="">
                 </div>
             </div>
         </div>
@@ -14,38 +14,38 @@
 
 <script>
 
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 export default {
     setup() {
 
-        const cards = ref([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        const images = ref([require('../assets/images/M.jpg'), require('../assets/images/restaurant_135621509.jpg'), require('../assets/images/Savage-2019-top-50-busy-restaurant.webp'), require('../assets/images/xmnsi-prince-restaurant-6803-hor-wide.webp')])
         const innerStyles = ref({})
         const step = ref('');
 
-        const inner = ref(null)
-
-        onMounted(() => {
-            const innerWidth = inner.value.scrollWidth;
-            const totalCards = cards.value.length;
-            step.value = `${innerWidth / totalCards}px`
-        })
+        const setStep = () => {
+            const innerWidth = document.getElementById('inner').offsetWidth;
+            console.log(innerWidth)
+            step.value = `${innerWidth}px`
+        }
 
         const prev = () => {
+            setStep()
             moveRight()
 
             afterTransition(() => {
-                const card = cards.value.pop()
-                cards.value.unshift(card)
+                const card = images.value.pop()
+                images.value.unshift(card)
                 resetTranslate()
             })
         }
 
         const next = () => {
+            setStep()
             moveLeft()
 
             afterTransition(() => {
-                const card = cards.value.shift()
-                cards.value.push(card)
+                const card = images.value.shift()
+                images.value.push(card)
                 resetTranslate()
             })
         }
@@ -53,9 +53,9 @@ export default {
         const afterTransition = (callback) => {
             const listener = () => {
                 callback()
-                inner.value.removeEventListener('transitionend', listener)
+                document.getElementById('inner').removeEventListener('transitionend', listener)
             }
-            inner.value.addEventListener('transitionend', listener)
+            document.getElementById('inner').addEventListener('transitionend', listener)
         }
 
         const moveLeft = () => {
@@ -77,7 +77,7 @@ export default {
             }
         }
 
-        return { cards, inner, next, prev, innerStyles }
+        return { images, next, prev, innerStyles }
     }
 }
 </script>
@@ -87,26 +87,30 @@ export default {
     overflow: hidden;
     display: flex;
     justify-content: center;
+    background-color: var(--mainColor);
 }
 
 .inner {
     white-space: nowrap;
     transition: transform 0.2s;
-
+    user-select: none;
 }
 
 .card {
-    width: 60px;
     margin-right: 10px;
     display: inline-flex;
-
-    height: 40px;
-    background-color: #39b1bd;
     color: white;
     border-radius: 4px;
-    align-items: center;
+    width: 100%;
     justify-content: center;
+
 }
+
+img {
+    max-width: 100%;
+    max-height: 500px;
+}
+
 
 .carousel-container {
     margin: 2rem;
