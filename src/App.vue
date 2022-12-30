5ab6ab35525488a8616addeffa09e5ff0514cb05
meszaros-adam <meszarosadam@outlook.hu>
2022-12-30 10:55:53 +0100

    up arrow

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,17 @@
           <contactVue></contactVue>
         </main>
       </div>
-      <footer> &#169; {{ year }}</footer>
+      <footer>
+        <div>&#169; {{ year }}</div>
+        <div>Kitalált Étterem</div>
+      </footer>
+      <i @click="scrollUp" class="bi bi-arrow-up-circle-fill hide" id="up-arrow"></i>
     </div>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import carouselVue from './components/carousel.vue'
 import navVue from './components/nav.vue'
 import openingVue from './components/opening.vue'
@@ -39,7 +43,29 @@ export default {
       return new Date().getFullYear()
     })
 
-    return { year }
+    const scrollUp = () => {
+      window.scrollTo(0, 0);
+    }
+
+    let upArrowId = null;
+
+    onMounted(() => {
+      upArrowId = document.getElementById("up-arrow")
+    })
+
+
+    const scrollFunc = () => {
+      const y = window.scrollY
+      if (y > 800) {
+        upArrowId.className = "bi bi-arrow-up-circle-fill show"
+      } else {
+        upArrowId.className = "bi bi-arrow-up-circle-fill hide"
+      }
+    }
+
+    window.addEventListener("scroll", scrollFunc)
+
+    return { year, scrollUp }
   }
 }
 </script>
@@ -53,6 +79,7 @@ export default {
   --tertiaryColor: #84757a;
   --shadow: 2px 2px grey;
   --borderRadius: 2rem;
+  --footerHeight: 3rem;
   font-weight: bolder;
 }
 
@@ -85,7 +112,7 @@ footer {
   bottom: 0;
   background: var(--mainColor);
   width: 100%;
-  height: 2.5rem;
+  height: var(--footerHeight);
   display: grid;
   place-items: center;
   font-weight: bolder;
@@ -101,8 +128,8 @@ main {
 }
 
 .content-wrap {
-  padding-bottom: 2.5rem;
-  /* Footer height */
+  padding-bottom: var(--footerHeight)
+    /* Footer height */
 }
 
 /* Utlities */
@@ -112,8 +139,31 @@ main {
   place-content: center;
 }
 
-.container{
+.container {
   margin: 3rem;
   padding: 3rem;
+}
+
+/* up arrow */
+#up-arrow {
+  position: fixed;
+  right: 2rem;
+  bottom: 2rem;
+  font-size: 2rem;
+  cursor: pointer;
+  transition: all ease 1s;
+}
+
+#up-arrow:hover {
+  color: #3C4048;
+}
+
+.hide {
+  opacity: 0;
+  left: -100%;
+}
+
+.show {
+  opacity: 1;
 }
 </style>
